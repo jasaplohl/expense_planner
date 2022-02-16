f87768e4cb9d813575753d4332755072b58c9f28
Jasa <jasa.plohl@gmail.com>
2022-02-16 10:01:09 +0000

    Transaction card style.

--- lib/transaction_card.dart
+++ lib/transaction_card.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
 class TransactionCard extends StatelessWidget {
 
   final Transaction transaction;
+
   final TextStyle amountStyle = TextStyle(
     color: Colors.red[300],
-    fontWeight: FontWeight.bold
+    fontWeight: FontWeight.bold,
+    fontSize: 20
   );
-  final EdgeInsets margin = const EdgeInsets.all(5);
+  final TextStyle itemStyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 15
+  );
+  final TextStyle dateStyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.grey,
+    fontSize: 13
+  );
+
+  final EdgeInsets margin = const EdgeInsets.symmetric(
+    vertical: 10,
+    horizontal: 15
+  );
 
   TransactionCard(this.transaction);
 
@@ -20,17 +36,25 @@ class TransactionCard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  transaction.title,
+                  style: itemStyle,
+                ),
+                Text(
+                  DateFormat.yMMMd().format(transaction.timeStamp),
+                  style: dateStyle,
+                ),
+              ],
+            ),
             margin: margin,
-            child: Text(transaction.timeStamp.toIso8601String()),
-          ),
-          Container(
-            margin: margin,
-            child: Text(transaction.title),
           ),
           Container(
             margin: margin,
             child: Text(
-              "-" + transaction.amount.toStringAsFixed(2),
+              "\$ -${transaction.amount.toStringAsFixed(2)}",
               style: amountStyle,
             ),
           )
